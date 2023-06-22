92ae03e7c50998cbc8fc12761bf5976e744692e3
ainugiri <40005294+ainugiri@users.noreply.github.com>
2023-06-22 12:05:53 +0530

    Update Test.java3

--- Test.java
+++ Test.java
@@ -9,13 +9,6 @@ public static void main(String[] args){
 	System.out.println(s3);
 	System.out.println(s4);
 	System.out.println(s4);
-	System.out.println(s1);
-	System.out.println(s2);
-	System.out.println(s3);
-	System.out.println(s4);
-	System.out.println(s4);
-	System.out.println(s1);
-	System.out.println(s2);
 	System.out.println(s3);
 	System.out.println(s4);
 	System.out.println(s4);
